74e4125a157d0b3b4f36c1e34f5242c18110e7be
Yurii Pertsev <pertsvuv@gmail.com>
2024-10-11 10:08:08 +0500

    Add REST routes

--- app/main.go
+++ app/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"todo-service/config"
 	"todo-service/internal/handler"
 	"todo-service/internal/repository"
 
+	"github.com/gin-gonic/gin"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
@@ -16,12 +16,17 @@ func main() {
 	dsn := "postgres://postgres:postgres@localhost:5432/todo?sslmode=disable"
 	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
 	db := bun.NewDB(sqldb, pgdialect.New())
-	// fmt.Println(db)
 
 	taskRepo := repository.NewTaskRepository(db)
+
+	router := gin.Default()
 
 	logger := config.InitLogger()
 	taskHandler := handler.NewTaskHandler(taskRepo, logger)
 
-	fmt.Println(taskHandler)
+	router.POST("/tasks", taskHandler.CreateTask)
+	router.GET("/tasks/:id", taskHandler.GetTask)
+	router.GET("/tasks", taskHandler.GetAllTasks)
+	router.PUT("/tasks/:id", taskHandler.UpdateTask)
+	router.DELETE("/tasks/:id", taskHandler.DeleteTask)
 }
